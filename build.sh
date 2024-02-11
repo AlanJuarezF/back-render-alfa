adf10493ea47c0cdc54cf71ea98431dd5ac5f593
Alan Juarez <alaanjuarezf@gmail.com>
2024-02-11 13:51:35 -0600

    administrator

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ python manage.py collectstatic
 python manage.py migrate
 
 # Crea el superusuario
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@gmail.com', '123456')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('administrator@gmail.com', '123456')" | python manage.py shell
